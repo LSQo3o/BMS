240a404f1fe02880fd851e4a7b4c65c664ac376e
LSQo3o <18743172435@163.com>
2023-05-28 15:13:40 +0800

    虚拟环境

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+"""
+虚拟环境的好处，就是保证我们项目环境的纯净，比如我在本地有多个项目，每个项目依赖的包不同，使用同一套环境比较乱，也不方便管理
+"""
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
